3f76cc253c8125a25aa56c9dd37f3936458e1177
cebehat <kontakt@cebt.dk>
2024-05-26 15:12:07 +0200

    beginning of attack functionality

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -19,16 +19,61 @@ namespace Cebt.LastStand
         [SerializeField] float smoothTime = 0.2f;
         [SerializeField] float inertia = 0.1f;
 
+        [Header("AttackSettings")]
+        [SerializeField] float attackRange = 5f;
+        [SerializeField] float attackCooldownTime = 2f;
+
         const float ZeroF = 0f;
 
         Vector3 movement;
         Vector2 mousePosition;
+
+        bool isAttacking = false;
+
+        CountdownTimer attackCooldown;
+        
 
         private void Awake()
         {
             rb.freezeRotation = true;
             Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true;
+
+            attackCooldown = new CountdownTimer(attackCooldownTime);
+        }
+
+        private void OnEnable()
+        {
+            input.Fire += OnFire;
+        }
+
+        private void OnDisable()
+        {
+            input.Fire -= OnFire;
+        }
+
+        private void OnFire(bool performed)
+        {
+            if (performed)
+            {
+                isAttacking = true;
+                HandleAttack();
+                attackCooldown.OnTimerStop += OnAttackCooldownEnd;
+            }
+            else
+            {
+                isAttacking = false;
+                attackCooldown.OnTimerStop -= OnAttackCooldownEnd;
+            }
+        }
+
+        private void OnAttackCooldownEnd()
+        {
+            if (isAttacking)
+            {
+                HandleAttack();
+                attackCooldown.Reset();
+            }
         }
 
 
@@ -43,12 +88,19 @@ namespace Cebt.LastStand
         {
             movement = new Vector3(input.Direction.x, ZeroF, input.Direction.y);
             mousePosition = new Vector2(input.MousePosition.x, input.MousePosition.y);
+            attackCooldown.Tick(Time.deltaTime);
         }
 
         private void FixedUpdate()
         {
             HandleMovement();
             HandleRotation();
+        }
+
+        private void HandleAttack()
+        {
+            attackCooldown.Start();
+            Debug.Log("Pew");
         }
 
         private void HandleMovement()
